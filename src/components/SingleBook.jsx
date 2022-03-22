3d3e4ccf1e900a8480a8fa8efe4ce88a8d3cc795
Oluwagbemi Oluwasakin <51876261+Gbemi06@users.noreply.github.com>
2022-03-22 14:56:26 +0200

    Initial commit

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,35 +1,28 @@
-import { Component } from "react";
+//import { Component } from "react";
 import { Card, Button } from "react-bootstrap";
-import CommentArea from "./CommentArea";
+//import CommentArea from "./CommentArea";
 import MyBadge from "./MyBadge";
 
-class SingleBook extends Component {
-  render() {
-    return (
-      <>
-        <Card
-          onClick={(e) => this.props.changeBook(this.props.selectedBook)}
-          style={{
-            border: !this.props.selectedBook ? "2px solid red" : "none",
-          }}
-        >
-          <MyBadge
-            color="black"
-            bgColor="yellow"
-            text="New"
-            padding={8}
-            position="absolute"
-          />
-          <Card.Img variant="top" src={this.props.img} />
+const SingleBook = (props) => {
+  return (
+    <>
+      <Card onClick={(e) => props.changeBook(props.selectedBook)}>
+        <MyBadge
+          color="black"
+          bgColor="yellow"
+          text="New"
+          padding={8}
+          position="absolute"
+        />
+        <Card.Img variant="top" src={props.img} />
 
-          <Card.Body>
-            <Card.Title>{this.props.title}</Card.Title>
-            <Button variant="primary">{this.props.price}</Button>
-          </Card.Body>
-        </Card>
-      </>
-    );
-  }
-}
+        <Card.Body>
+          <Card.Title>{props.title}</Card.Title>
+          <Button variant="primary">{props.price}</Button>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
 
 export default SingleBook;
